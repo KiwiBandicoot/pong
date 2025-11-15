ae31e08dd6c285d681b9473a0fb691bac30616d4
David Hocking <65630382+KiwiBandicoot@users.noreply.github.com>
2025-11-15 18:47:55 +1300

    Spawn paddle functions

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,20 @@ fn main() {
     app.run();
 }
 
+#[derive(Component)]
+struct Paddle {
+    move_up: KeyCode,
+    move_down: KeyCode,
+}
+
+fn spawn_players(mut commands: Commands) {
+    commands.spawn(ImageBundle {
+        transform: Transform::from_translation(Vec3::new(-300., 0., 0.)),
+        background_color: BackgroundColor(Color::WHITE),
+        style: Style {
+            width: Val::Px(10.),
+            height: Val::Px(150.),
+        },
+        ..Default::default()
+    })
+}
